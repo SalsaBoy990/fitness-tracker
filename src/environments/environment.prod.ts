5dec6adb17fb368f06ad99cd4c5ab9f7f0c55214
Andras Gulacsi <guland@protonmail.com>
2020-09-20 21:39:17 +0200

    configure app to publish it on netlify

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB44ui3v4FutB-Y2e0WFuATpeb5v-Vgc2Y",
+    authDomain: "ng-fitness-tracker-e4c57.firebaseapp.com",
+    databaseURL: "https://ng-fitness-tracker-e4c57.firebaseio.com",
+    projectId: "ng-fitness-tracker-e4c57",
+    storageBucket: "ng-fitness-tracker-e4c57.appspot.com",
+    messagingSenderId: "862058668463",
+    appId: "1:862058668463:web:ef3b5434ae4b1a9babcf5a"
+  }
 };
